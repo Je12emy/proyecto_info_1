8c5801f7a3a720b6fabc2517c54749d47e3da47f
Je12emy <jeremyzelaya@hotmail.es>
2021-12-07 23:11:10 -0600

    Finish main program

--- docs/index.md
+++ docs/index.md
@@ -8,14 +8,14 @@ El programa de inicio utiliza un ciclo `do` junto a una variable booleana `run`,
 
 Aca se le presenta al usuario una lista de programas disponibles y se captura la opción ingresada, este valor se evalua con una estructura de control `switch` para ejecutar cada ejercicio.
 
-![Switch Principal](resources/switch_principal.png)
+![Switch Principal](resources/switch_programa.png)
 
 
 ## Programa de Triangulos
 
 El primer programa empieza capturando las 3 variables: `a`, `b` y `c`, de tipo `double` para no perder presición al momento de calcular el area del triangulo. Estas variables son entonces evualuadas en un condicional para verificar si una de ellas es negativa o igual a 0, de ser verdad entonces se muestrael error "¡Solo se aceptan valores positivos!", de ser falso se continua con otro condicional para evaluar si los valores ingresados crean un triangulo: `(a + b) > c`, de ser falso se muestra el error: "¡Los valores ingresados no crean un triangulo!". Con estos valores ya evaluados se calcula la formula de herron para obtener el area del triangulo.
 
-![Formula Herron](resources/triagulo_area.png)
+![Formula Herron](resources/triangulo_area.png)
 
 Con el calculo del area completo, se evalua el tipo de triangulo con las medidas ingresadas.
 
--- main.cpp
+++ main.cpp
@@ -31,7 +31,10 @@ int main() {
 
     // Ejecutar programa con un switch
     switch (opcion) {
-    case 'A':
+    case 'A': {
+      // Reiniciar la cantidad de intentos posibles
+      intentos = 3;
+
       cout << "Calculador de area de un triangulo" << endl;
       // Capturar el largo de cada lado
       double a, b, c;
@@ -79,15 +82,146 @@ int main() {
         }
       }
       break;
-    case 'B':
-      cout << "Programa B" << endl;
+    }
+    case 'B': {
+
+      // Reiniciar la cantidad de intentos posibles
+      intentos = 3;
+
+      cout << "Calculador de primedio final." << endl;
+
+      // Programa Calculador de promedio
+      string nombre;
+      cout << "Ingrese un nombre: ";
+      cin >> nombre;
+
+      float nota1, nota2, nota3, nota4, nota5, nota6;
+      float porcentaje1, porcentaje2, porcentaje3, porcentaje4, porcentaje5,
+          porcentaje6;
+      float resultado;
+
+      // Obtener valores
+      cout << "- Ingrese un valor para el primer instrumento: ";
+      cin >> nota1;
+      cout << "+ Ingrese un porcentaje para el primer instrumento: ";
+      cin >> porcentaje1;
+
+      cout << "- Ingrese un valor para el segundo instrumento: ";
+      cin >> nota2;
+      cout << "+ Ingrese un porcentaje para el segundo instrumento: ";
+      cin >> porcentaje2;
+
+      cout << "- Ingrese un valor para el tercer instrumento: ";
+      cin >> nota3;
+      cout << "+ Ingrese un porcentaje para el tercer instrumento: ";
+      cin >> porcentaje3;
+
+      cout << "- Ingrese un valor para el cuarto instrumento: ";
+      cin >> nota4;
+      cout << "+ Ingrese un porcentaje para el cuarto instrumento: ";
+      cin >> porcentaje4;
+
+      cout << "- Ingrese un valor para el quinto instrumento: ";
+      cin >> nota4;
+      cout << "+ Ingrese un porcentaje para el quinto instrumento: ";
+      cin >> porcentaje4;
+
+      cout << "- Ingrese un valor para el sexto instrumento: ";
+      cin >> nota6;
+      cout << "+ Ingrese un porcentaje para el sexto instrumento: ";
+      cin >> porcentaje6;
+
+      cout << "Calculando resultados....";
+
+      // Calcular promedio ponderado
+      // Fuente: https://es.wikihow.com/calcular-el-promedio-ponderado
+      resultado = (nota1 * porcentaje1 / 100) + (nota2 * porcentaje2 / 100) +
+                  (nota3 * porcentaje3 / 100) + (nota4 * porcentaje4 / 100) +
+                  (nota5 * porcentaje5 / 100) + (nota6 * porcentaje6 / 100);
+
+      cout << endl;
+      cout << "Resultados: " << endl;
+      cout << "~ La calificación de " << nombre << " es " << resultado << endl;
+
+      // Evaluar resultado
+      if (resultado >= 70) {
+        cout << nombre << " ha aprobado el curso!" << endl;
+      } else {
+        cout << nombre << " ha reprobado el curso" << endl;
+      }
       break;
-    case 'C':
-      cout << "Programa C" << endl;
+    }
+    case 'C': {
+      // Reiniciar la cantidad de intentos posibles
+      intentos = 3;
+
+      // Programa Contador de Dígitos
+      cout << "Contador de dígitos." << endl;
+      string numero;
+
+      // Capturar la entrada
+      cout << "Ingrese un numero de máximo 4 dígitos: ";
+      cin >> numero;
+
+      int tamaño = numero.length();
+      // Con la funcion length es posible obtner el tamaño de una cadena de text
+      if (tamaño <= 4) {
+        // Variables acumuladores
+        int suma = 0;
+        // Variable temporal para guardar el valor de un item dentro de la
+        // cadena de texto
+        string temp;
+        // Iterar por cada item en la cadena de texto
+        for (int i = 0; i < tamaño; i++){  
+          // Guardar el valor actual
+          temp = numero[i];
+          suma = suma + stoi(temp);
+        }
+        // Mostrar resultados
+        cout << endl;
+        cout << "~ Suma de los dígitos: " << suma << endl;
+        cout << "~ Cantidad de dígitos: " << tamaño << endl;
+        cout << endl;
+
+      } else {
+        cout << "Ese numero no es valido..." << endl;
+      }
       break;
-    default:
-      cout << "Salida" << endl;
+    }
+
+    case 'D': {
+      intentos = 3;
+      char salir;
+      cout << "¿Desea salir del programa?[s/n]: ";
+      cin >> salir;
+
+      switch (salir) {
+      case 's': {
+        cout << "Cerrando programa...." << endl;
+        run = false;
+        break;
+      }
+      case 'n': {
+        break;
+      }
+      default: {
+        cout << "Esa no es una opción!" << endl;
+      }
+      }
       break;
+    }
+
+    default: {
+      // Reducir la cantidad de intentos
+      intentos = intentos - 1;
+      cout << "Esa no es una opción valida... Intentos restantes " << intentos 
+           << endl;
+      if (intentos == 0) {
+        cout << "Ha llegado al limite de intentos, cerrando programa.." << endl;
+        run = false;
+      }
+      break;
+    }
     }
   } while (run);
 }
